df47e44c73e597b94bfaf8e7208512169f1a4672
Zaknev <hcml474@gmail.com>
2022-08-24 18:48:09 +0100

    create componio

---
+++ src/componio.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const Componio = ({ nomee, idadee }) => {
+  return (
+    <div>
+      <h1>{nomee}</h1>
+      <h3>{idadee}</h3>
+    </div>
+  );
+};
+
+export default Componio;
